44ab05771b6dca8da5548bab7fc1b34f65b2320c
lucasleal-developer <lucasleal.developer@gmail.com>
2025-04-05 22:46:15 -0300

    Configura aliases no Vite e ajusta build

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
+      "@": path.resolve(__dirname, "./client/src"),
+      "@shared": path.resolve(__dirname, "./shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
