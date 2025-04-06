f45ed796fa5acbc9abe19a7e67f7ed1dc8a9349b
lucasleal-developer <lucasleal.developer@gmail.com>
2025-04-05 22:50:03 -0300

    Separa build do cliente e servidor, ajusta configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(__dirname, "dist/client"),
     emptyOutDir: true,
   },
   server: {
